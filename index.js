597b5853983193ee3a71e12a6f8c4480aa875148
mmalongg <mmalong2496@sdsu.edu>
2023-10-10 18:47:45 -0700

    created toggleDarkMode button

---
+++ index.js
@@ -0,0 +1,11 @@
+let themeButton = document.getElementById("theme-button");
+
+// TODO: Complete the toggleDarkMode function
+const toggleDarkMode = () => {
+    // Write your code to manipulate the DOM here
+    document.body.classList.toggle("dark-mode");
+
+}
+themeButton.addEventListener("click", toggleDarkMode);
+
+
