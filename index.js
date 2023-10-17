0628f0d7384b25845e43fa570942cbf1af929f85
mmalongg <mmalong2496@sdsu.edu>
2023-10-16 22:46:11 -0700

    update

--- index.js
+++ index.js
@@ -8,4 +8,25 @@ const toggleDarkMode = () => {
 }
 themeButton.addEventListener("click", toggleDarkMode);
 
+var i = 0;
+var txt = 'Distractions are everwhere! Learn more about your media consumption today!'; /* The text */
+var speed = 100; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("typewriter").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  } else {
+    // After all text has been typed out, wait for some time then reset and start over
+    setTimeout(function() {
+      document.getElementById("typewriter").innerHTML = '';
+      i = 0;
+      typeWriter();
+    }, 5000); // 2000ms = 2s delay
+  }
+}
+
+window.onload = typeWriter;
+
 
